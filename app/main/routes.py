1ede802b86291898bc0991a809c86379ccf6c6d6
Gwyn Jones <gwyn.jones@nationalarchives.gov.uk>
2025-07-18 12:27:32 +0100

    Introduce mechanism for loading content from YAML files
    This commit introduces the load_content function that will retrieve
    structured content from a YAML file and make it available as a Python
    object, crucially with properties available via dot notation.
    
    The content obtained by load_content is then passed to the template
    via render_template.
    
    The intent of using YAML to store the content is that:
    
    1. It allows us to keep all the content together and structured in
       a way that reflects its inherent organisation
    2. It avoids the need for content to be stored in Python code or
       HTML templates, making content review and management easier.

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,12 +1,14 @@
 from app.lib.cache import cache, cache_key_prefix
 from app.main import bp
 from flask import render_template
+from app.lib.content import load_content
 
 
 @bp.route("/")
 @cache.cached(key_prefix=cache_key_prefix)
 def index():
-    return render_template("main/index.html")
+    content = load_content()
+    return render_template("main/index.html", content=content)
 
 
 @bp.route("/cookies/")
