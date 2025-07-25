15975114f6ccce5da1442e70ec75ef81b6d7c07f
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2025-07-25 17:57:50 +0100

    Create separate lib file for creating payment links

--- app/main/routes.py
+++ app/main/routes.py
@@ -5,7 +5,7 @@ from app.main import bp
 from app.main.forms.request_a_service_record import RequestAServiceRecord
 from config import Base
 from flask import redirect, render_template, session, url_for
-
+from app.lib.gov_uk_pay import create_payment
 
 @bp.route("/")
 @cache.cached(key_prefix=cache_key_prefix)
@@ -25,33 +25,25 @@ def request_form():
             if field_name not in ["csrf_token", "submit", "evidence_of_death"]:
                 session["form_data"][field_name] = field.data
 
-        headers = {
-            "Authorization": f"Bearer {Base.GOV_UK_PAY_API_KEY}",
-            "Content-Type": "application/json",
-        }
+        response = create_payment(
+            amount=1000,
+            description="Request a Service Record",
+            reference="ServiceRecordRequest",
+            return_url=url_for("main.submitted", _external=True),
+        )
 
-        payload = {
-            "amount": 1000,
-            "description": "Request a Service Record",
-            "reference": "ServiceRecordRequest",  # TODO: Investigate the dynamic reference that other/current services use (TNA-xxxxx?)
-            "return_url": url_for("main.submitted", _external=True),
-        }
-
-        response = requests.post(Base.GOV_UK_PAY_API_URL, json=payload, headers=headers)
-
-        if response.status_code != 201:
-            print("Error creating payment:", response.status_code, response.json())
+        if not response:
             return render_template(
                 "main/request-a-service-record.html",
                 content=content,
                 form=form,
-                error="Payment creation failed. Please try again.",
+                error="Payment link creation failed. Please try again.",
             )
         else:
             session["payment_url"] = (
-                response.json().get("_links", {}).get("next_url", "").get("href", "")
+                response.get("_links", {}).get("next_url", "").get("href", "")
             )
-            session["payment_id"] = response.json().get("payment_id", "")
+            session["payment_id"] = response.get("payment_id", "")
             return redirect(session["payment_url"])
 
     return render_template(
