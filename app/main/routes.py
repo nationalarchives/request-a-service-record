afd1971a6cb8403a661c0de3e20a2b6fcb03d7d0
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2025-07-25 17:16:01 +0100

    Basic GOV.UK Pay integration

--- app/main/routes.py
+++ app/main/routes.py
@@ -3,6 +3,8 @@ from app.lib.content import load_content
 from app.main import bp
 from app.main.forms.request_a_service_record import RequestAServiceRecord
 from flask import redirect, render_template, session, url_for
+from config import Base
+import requests
 
 
 @bp.route("/")
@@ -23,7 +25,31 @@ def request_form():
             if field_name not in ["csrf_token", "submit", "evidence_of_death"]:
                 session["form_data"][field_name] = field.data
 
-        return redirect(url_for("main.submitted"))
+        headers = {
+            "Authorization": f"Bearer {Base.GOV_UK_PAY_API_KEY}",
+            "Content-Type": "application/json"
+        }
+
+        payload = {
+            "amount": 1000,
+            "description": "Request a Service Record",
+            "reference": "ServiceRecordRequest", # TODO: Investigate the dynamic reference that other/current services use (TNA-xxxxx?)
+            "return_url": url_for("main.submitted", _external=True),
+        }
+
+        response = requests.post(
+            Base.GOV_UK_PAY_API_URL,
+            json=payload,
+            headers=headers
+        )
+
+        if response.status_code != 201:
+            print("Error creating payment:", response.status_code, response.json())
+            return render_template("main/request-a-service-record.html", content=content, form=form, error="Payment creation failed. Please try again.")
+        else:
+            session["payment_url"] = response.json().get("_links", {}).get("next_url", "").get("href", "")
+            session["payment_id"] = response.json().get("payment_id", "")
+            return redirect(session["payment_url"])
 
     return render_template(
         "main/request-a-service-record.html", content=content, form=form
