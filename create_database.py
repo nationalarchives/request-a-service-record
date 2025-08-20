4085f5a6e1fe7945bc22582988b985bf0cb1655a
James Biggs <62654785+jamesbiggs@users.noreply.github.com>
2025-08-20 16:46:37 +0100

    Update create_database.py
    Co-authored-by: Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>

--- create_database.py
+++ create_database.py
@@ -1,5 +1,6 @@
 from app.lib.models import db
 from flask_app import app
 
-with app.app_context():
-    db.create_all()
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
