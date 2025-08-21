7e4b798f1ee81e6125cf325a342ddc98151e1314
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2025-08-21 15:03:50 +0100

    Add AWS_SESSION_TOKEN

--- config.py
+++ config.py
@@ -69,6 +69,7 @@ class Production(Features):
     AWS_ACCESS_KEY_ID: str = os.environ.get("AWS_ACCESS_KEY_ID", "")
     AWS_SECRET_ACCESS_KEY: str = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
     AWS_DEFAULT_REGION: str = os.environ.get("AWS_DEFAULT_REGION", "")
+    AWS_SESSION_TOKEN: str = os.environ.get("AWS_SESSION_TOKEN", "")
     S3_BUCKET_NAME: str = os.environ.get("S3_BUCKET_NAME", "")
 
 
