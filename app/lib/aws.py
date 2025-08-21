17e27c9e7dedbc60e369fae2b02af906f9bab6aa
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2025-08-21 15:10:56 +0100

    Apply AWS_SESSION_TOKEN and include file extension in filename

--- app/lib/aws.py
+++ app/lib/aws.py
@@ -1,6 +1,6 @@
-
 import boto3
 import uuid
+import os 
 
 from flask import current_app
 
@@ -10,12 +10,20 @@ def file_upload_to_proof_of_death_storage(file) -> str | None:
             "s3",
             aws_access_key_id=current_app.config["AWS_ACCESS_KEY_ID"],
             aws_secret_access_key=current_app.config["AWS_SECRET_ACCESS_KEY"],
+            aws_session_token=current_app.config.get("AWS_SESSION_TOKEN"),
             region_name=current_app.config.get("AWS_REGION", "eu-west-2"),
         )
+        
         bucket_name = current_app.config["S3_BUCKET_NAME"]
-        
-        filename = str(uuid.uuid4())
-        
-        s3.upload_fileobj(file, bucket_name, filename)
+
+        file_extension = os.path.splitext(file.filename)[1]
+
+        filename = str(uuid.uuid4()) + file_extension
+
+        try:
+            s3.upload_fileobj(file, bucket_name, filename)
+        except Exception as e:
+            current_app.logger.error(f"Error uploading file to S3: {e}")
+            return None
 
         return filename
